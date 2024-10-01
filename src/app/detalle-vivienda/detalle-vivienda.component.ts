812aec94e0c82cf5ea797d974d2b7e227718f1ac
Briyan Anthony Dávila Ortiz <bdavilao2@upao.edu.pe>
2024-09-30 21:59:05 -0500

    cambio de clase

--- src/app/detalle-vivienda/detalle-vivienda.component.ts
+++ src/app/detalle-vivienda/detalle-vivienda.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router'; // Importar Router
 import { Property } from '../services/propertyService/property.model';
 import { PropertyService } from '../services/propertyService/property.service';
+import { UserService } from '../services/userService/user.service';
+import { User } from '../services/userService/user.model';
 
 @Component({
   selector: 'app-detalle-vivienda',
@@ -11,8 +13,9 @@ import { PropertyService } from '../services/propertyService/property.service';
 export class DetalleViviendaComponent implements OnInit {
   viviendaId!: number;
   vivienda!: Property; // Cambia a tipo Property
+  usuario!: User;
 
-  constructor(private route: ActivatedRoute, private router: Router, private propertyService: PropertyService) {}
+  constructor(private route: ActivatedRoute, private router: Router, private propertyService: PropertyService, private userService: UserService) {}
 
   ngOnInit(): void {
     this.viviendaId = Number(this.route.snapshot.paramMap.get('id'));
@@ -24,6 +27,16 @@ export class DetalleViviendaComponent implements OnInit {
       console.error('Error fetching property details:', error);
       this.regresar(); // Redirigir si hay un error
     });
+
+    if(this.vivienda.userId){
+      this.userService.getById(this.vivienda.userId).subscribe(data => {
+        this.usuario= data;
+      }, error => {
+        console.error('Error fetching user details:', error);
+        this.regresar(); // Redirigir si hay un error
+      });
+    }
+    
   }
 
   regresar() {
